27c98ddac1228bd83b0d1212ae77cda2c4fed576
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2026-01-16 04:59:48 +0530

    add auth file

---
+++ src/pages/auth/index.ts
@@ -0,0 +1,9 @@
+// src/pages/auth/index.ts
+// Export all auth pages for easy imports
+
+export { default as AuthLayout } from './AuthLayout';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as ForgotPassword } from './ForgotPassword';
+export { default as ResetPassword } from './ResetPassword';
+export { default as VerifyEmail } from './VerifyEmail';
