e18da2f1f549f9c7dc7686377233fdb81f2bb688
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2026-01-16 04:10:20 +0530

    make dashboard mobile view

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,23 +1,39 @@
 // src/pages/dashboard/Dashboard.tsx
 
+import { useState } from "react";
 import Filter from "../../components/filters/Filter";
 import Sidebar from "../../components/sidebar/Sidebar";
 import DataTable from "../../components/table/DataTable";
 import Topbar from "../../components/topbar/Topbar";
 
+const Dashboard = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-const Dashboard = () => (
-  <div className="flex h-screen">
-    <Sidebar />
-    <div className="flex-1 flex flex-col">
-      <Topbar />
-      <main className="p-6 bg-gray-50 flex-1 flex flex-col gap-2 ">
-        <Filter />
-        <DataTable />
-        {/* Add pagination here */}
-      </main>
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+  const closeSidebar = () => setSidebarOpen(false);
+
+  return (
+    <div className="flex h-screen">
+      {/* Mobile Overlay */}
+      <div
+        className={`sidebar-overlay md-hidden ${sidebarOpen ? "active" : ""}`}
+        onClick={closeSidebar}
+      />
+
+      {/* Sidebar - responsive */}
+      <Sidebar isOpen={sidebarOpen} onClose={closeSidebar} />
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col min-w-0">
+        <Topbar onMenuClick={toggleSidebar} />
+        <main className="p-6 bg-page flex-1 flex flex-col gap-2 main-mobile overflow-auto">
+          <Filter />
+          <DataTable />
+          {/* Add pagination here */}
+        </main>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Dashboard;
